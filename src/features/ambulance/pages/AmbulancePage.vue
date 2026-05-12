9cab4c9f910ba1f750d0af05a6d12b5402fdb130
navysingchai <navysingchai@gmail.com>
2026-05-12 16:40:50 +0700

    added test page route for test

--- src/features/ambulance/pages/AmbulancePage.vue
+++ src/features/ambulance/pages/AmbulancePage.vue
@@ -2,6 +2,6 @@
   <div class="p-8 text-emergency">
     <h1 class="text-2xl font-bold">Emergency Ambulance</h1>
     <p class="mt-4 text-muted-foreground">Fast response emergency services.</p>
-    <router-link to="/" class="text-primary underline mt-4 block text-foreground">Back to Home</router-link>
+    <router-link to="/" class="text-primary underline mt-4 block">Back to Home</router-link>
   </div>
 </template>
---
+++ src/features/test/pages/TestPage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Avatar from '@/shared/components/ui/Avatar.vue'
+import AvatarImage from '@/shared/components/ui/AvatarImage.vue';
+import doctorAvatar from '@/assets/doctor-avartar.png'
+import Button from '@/shared/components/ui/Button.vue';
+import Card from '@/shared/components/ui/Card.vue';
+import CardHeader from '@/shared/components/ui/CardHeader.vue';
+import CardTitle from '@/shared/components/ui/CardTitle.vue';
+import CardContent from '@/shared/components/ui/CardContent.vue';
+import Input from '@/shared/components/ui/Input.vue';
+</script>
+
+<template>
+  <Avatar>
+    <AvatarImage :src="doctorAvatar"></AvatarImage>
+  </Avatar>
+  <Button variant="ghost">test btn</Button>
+  <Card>
+    <CardHeader>
+      <CardTitle>test</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <span class="mb-3">Enter your age</span>
+      <Input type="number" class="mt-3" placeholder="Enter your age"  />
+      <Button class="mt-3">Submit</Button>
+    </CardContent>
+  </Card>
+</template>
