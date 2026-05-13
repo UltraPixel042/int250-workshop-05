7a64ea8cd751a6b6399de9360b9a0516b7b4e63d
thanawat li <thanawat@example.com>
2026-05-13 10:21:19 +0700

    mod card

--- src/features/medical/pages/MedicalPage.vue
+++ src/features/medical/pages/MedicalPage.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="">
+  <div class=" border   rounded-t-3xl space-y-10 ">
+
+    <div class="bg-green-700 h-32 p-5   rounded-t-3xl flex  flex-col space-y-1"> 
+       <h1 class="text-sm font-normal text-white">MEDICINE SERVICES</h1>
+     <h1 class="text-2xl font-semibold text-white">Buy Medicine</h1>
+     <h1 class="text-sm font-extralight text-white">You can buy Non-prescription medication here.</h1>
+    
+  </div>
+
+   
     <!-- <h1 class="text-2xl font-bold">Medical Records & Services</h1>
     <p class="mt-4 text-muted-foreground">Access your health records and medical history.</p> -->
+    <div class="p-3">
 
-    <div class="border rounded-lg w-lg  p-3 shadow-md flex flex-col justify-between">
-      <h1 class="text-2xl font-semibold text-green-700">Best Selling</h1>
-      <img src="../../../assets/apache.png" class=" max-w-[80%] mx-auto" alt="ยาแก้ไอ">
+    <div class="border rounded-2xl  p-3  shadow-md flex flex-col justify-between bg-green-50">
+      
+      <h1 class="text-3xl font-extrabold text-lime-600 ">Best Selling</h1>
+      
+      
+      <img src="../../../assets/apache.png" class=" max-w-[55%] mx-auto" alt="ยาแก้ไอ">
       <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-      <a href="" class=" border rounded-lg  py-2 bg-green-700 text-white text-center mt-auto mx-auto w-xs ">View Detail </a>
+      <a href="" class=" border rounded-2xl  py-2 bg-green-600 text-white text-center mt-5 mx-auto w-xs font-bold ">View Detail </a>
+      
     </div>
 
     <div>
@@ -17,10 +31,10 @@
       <div class="flex overflow-x-auto gap-4">
 
 
-        <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
           <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
           <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-          <a href="" class=" border rounded-lg text-xs  py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs">View Detail
+          <a href="" class=" border rounded-2xl text-xs font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">View Detail
           </a>
         </div>
 
@@ -28,18 +42,18 @@
 
 
         
-          <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
             <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
             <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-lg text-xs py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs ">View Detail
+            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs ">View Detail
             </a>
           </div>
 
 
-          <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
             <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
             <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-lg text-xs py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs">View Detail
+            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
             </a>
           </div>
   </div>
@@ -51,10 +65,10 @@
       <div class="flex overflow-x-auto gap-4">
 
 
-        <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
           <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
           <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-          <a href="" class=" border rounded-lg text-xs  py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs">View Detail
+          <a href="" class=" border rounded-2xl text-xs font-bold  py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
           </a>
         </div>
 
@@ -62,18 +76,18 @@
 
 
         
-          <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
             <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
             <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-lg text-xs py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs ">View Detail
+            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs ">View Detail
             </a>
           </div>
 
 
-          <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
             <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
             <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-lg text-xs py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs">View Detail
+            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
             </a>
           </div>
 
@@ -87,10 +101,10 @@
       <div class="flex overflow-x-auto gap-4">
 
 
-        <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
           <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
           <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-          <a href="" class=" border rounded-lg text-xs  py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs">View Detail
+          <a href="" class=" border rounded-2xl text-xs font-bold  py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
           </a>
         </div>
 
@@ -98,25 +112,25 @@
 
 
         
-          <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
             <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
             <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-lg text-xs py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs ">View Detail
+            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs ">View Detail
             </a>
           </div>
 
 
-          <div class="border rounded-lg w-sm  p-3 shadow-md shrink-0 flex flex-col justify-between">
+          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
             <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
             <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-lg text-xs py-2 bg-green-700 text-white block  text-center mt-auto mx-auto w-3xs">View Detail
+            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
             </a>
           </div>
 
  </div>
 
 
-
+</div>
 
 
 
