da8a649296c9208ac34dd29f067dacf7ef4ea713
thanawat li <thanawat@example.com>
2026-05-13 15:05:30 +0700

    mod medicine more

--- src/features/medical/pages/MedicalPage.vue
+++ src/features/medical/pages/MedicalPage.vue
@@ -18,118 +18,196 @@
       <h1 class="text-3xl font-extrabold text-lime-600 ">Best Selling</h1>
       
       
-      <img src="../../../assets/apache.png" class=" max-w-[55%] mx-auto" alt="ยาแก้ไอ">
-      <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-      <a href="" class=" border rounded-2xl  py-2 bg-green-600 text-white text-center mt-5 mx-auto w-xs font-bold ">View Detail </a>
+      <img src="../../../assets/tylenol.webp" class=" max-w-[55%] mx-auto" alt="ยาแก้ไอ">
+      <p class="text-center font-semibold">Tylenol</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน: ลดไข้ บรรเทาอาการปวดหัว ปวดตัว ระดับเล็กน้อยถึงปานกลาง</p>
+      <a href="" class=" border rounded-2xl  py-2 bg-green-600 text-white text-center mt-5 mx-auto w-xs font-bold ">Buy Now!</a>
       
     </div>
 
+
+<!-- --------------------------------------------category Pain Relief & Fever---------------------------------------------------------------------- -->
     <div>
-      <h1  class=" border-b-2  w-3xs  my-5">Pain Relief & Fever</h1>
+      <h1  class=" border-b-2 border-green-200  w-3xs  my-5 font-extrabold text-lime-600">Pain Relief & Fever</h1>
         
-      <!-- ------------------------------category----------------------------------------------------- -->
+    
       <div class="flex overflow-x-auto gap-4">
 
 
-        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-          <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-          <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-          <a href="" class=" border rounded-2xl text-xs font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">View Detail
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0  flex flex-col justify-between">
+          <img src="../../../assets/tylenol.webp" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Tylenol</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน: ลดไข้ บรรเทาอาการปวดหัว ปวดตัว ระดับเล็กน้อยถึงปานกลาง</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
           </a>
         </div>
 
 
-
-
-        
-          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-            <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-            <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs ">View Detail
-            </a>
-          </div>
-
-
-          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-            <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-            <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
-            </a>
-          </div>
-  </div>
-
-  <!-- ------------------------------category2----------------------------------------------------- -->
-      <h1  class=" border-b-2  w-3xs  my-5">Pain Relief & Fever</h1>
-        
-      <!-- ------------------------------category----------------------------------------------------- -->
-      <div class="flex overflow-x-auto gap-4">
-
-
         <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-          <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-          <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-          <a href="" class=" border rounded-2xl text-xs font-bold  py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
+          <img src="../../../assets/nurofen.webp" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Nurofen</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน: บรรเทาอาการไข้สูง ปวดประจำเดือน ปวดฟัน หรือปวดกล้ามเนื้ออักเสบ</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
           </a>
         </div>
 
 
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Decolgen.webp" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Decolgen</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:บรรเทาอาการหวัด ร่วมกับอาการไข้ ปวดศีรษะ และมีน้ำมูก</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+     </div>
+</div>
 
 
+<!-- --------------------------------------------category Cold, Cough & Allergy--------------------------------------------------------------------- -->
+    <div>
+      <h1  class=" border-b-2 border-green-200  w-3xs  my-5 font-extrabold text-lime-600">Cold, Cough & Allergy</h1>
         
-          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-            <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-            <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs ">View Detail
-            </a>
-          </div>
-
-
-          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-            <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-            <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
-            </a>
-          </div>
-
- </div>
-
- 
-  <!-- ------------------------------category2----------------------------------------------------- -->
-      <h1  class=" border-b-2  w-3xs  my-5">Pain Relief & Fever</h1>
-        
-      <!-- ------------------------------category----------------------------------------------------- -->
+    
       <div class="flex overflow-x-auto gap-4">
 
 
-        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-          <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-          <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-          <a href="" class=" border rounded-2xl text-xs font-bold  py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0  flex flex-col justify-between">
+          <img src="../../../assets/Chlorpheniramine.webp" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Chlorpheniramine</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ช่วยลดน้ำมูกไหล คันคอ และจามจากหวัดหรือภูมิแพ้ได้ดีเยี่ยม</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
           </a>
         </div>
 
 
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Cetirizine.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Cetirizine</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ยาแก้แพ้รุ่นที่ 2 ออกฤทธิ์นาน 24 ชั่วโมง ช่วยลดอาการจาม น้ำมูกไหล และคันตาจากภูมิแพ้</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
 
 
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Fluimucil.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Fluimucil</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ยาละลายเสมหะข้นเหนียวในหลอดลม ทำให้เสมหะใสขึ้นและขับออกด้วยการไอได้ง่ายขึ้น</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+     </div>
+</div>
+
+
+<!-- --------------------------------------------category Digestive Health--------------------------------------------------------------------- -->
+    <div>
+      <h1  class=" border-b-2 border-green-200  w-3xs  my-5 font-extrabold text-lime-600">Digestive Health</h1>
         
-          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-            <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-            <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs ">View Detail
-            </a>
-          </div>
+    
+      <div class="flex overflow-x-auto gap-4">
 
 
-          <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
-            <img src="../../../assets/apache.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
-            <p class="text-center">ยาแก้ไอโยคีในรัศมีวงกลม</p>
-            <a href="" class=" border rounded-2xl text-xs font-bold py-3 bg-green-600 text-white block  text-center mt-5 mx-auto w-3xs">View Detail
-            </a>
-          </div>
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0  flex flex-col justify-between">
+          <img src="../../../assets/Gaviscon.webp" class=" max-w-[50%] mx-auto" alt="Gaviscon">
+          <p class="text-center font-semibold">Gaviscon</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:มีสารลดกรดที่เป็นด่างช่วยปรับสมดุลกรดในกระเพาะอย่างรวดเร็ว และบางสูตรช่วยสร้างชั้นเจลบล็อกไม่ให้กรดไหลย้อนขึ้นมาที่หลอดลม</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
 
- </div>
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Air-X.webp" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Air-X</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน: ยาธาตุน้ำแดงช่วยขับลมและกระตุ้นการย่อยอาหาร ส่วนยาที่มีไซเมทิโคน (Simethicone) จะช่วยกระจายฟองแก๊สขนาดใหญ่ในกระเพาะให้เล็กลง ทำให้ขับลมออกมาระหว่างวันได้ง่ายขึ้น (เรอหรือตด)</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
 
 
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Smecta.webp" class=" max-w-[50%] mx-auto" alt="Smecta">
+          <p class="text-center font-semibold">Smecta</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ช่วยย่อยโปรตีน คาร์โบไฮเดรต และไขมัน ลดภาระการทำงานของตับอ่อนและกระเพาะอาหาร</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+     </div>
+</div>
+
+
+<!-- --------------------------------------------category First Aid & Skin Care--------------------------------------------------------------------- -->
+    <div>
+      <h1  class=" border-b-2 border-green-200  w-3xs  my-5 font-extrabold text-lime-600">First Aid </h1>
+        
+    
+      <div class="flex overflow-x-auto gap-4">
+
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0  flex flex-col justify-between">
+          <img src="../../../assets/Ethyl.webp" class=" max-w-[50%] mx-auto" alt="Gaviscon">
+          <p class="text-center font-semibold">Ethyl Alcohol 70%</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ฆ่าเชื้อโรค รอบบาดแผล หรือล้างมือและเครื่องมือก่อนทำแผล</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Normal.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Normal Saline 0.9%</p>
+          <p class="text-center text-xs text-gray-500 font-light">ล้างทำความสะอาดแผลสด แผลถลอก แผลไฟไหม้</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Betadine.png" class=" max-w-[50%] mx-auto" alt="Smecta">
+          <p class="text-center font-semibold">Betadine</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ฆ่าเชื้อโรคที่บาดแผลสด แผลถลอก หรือแผลมีดบาด</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+     </div>
+</div>
+
+
+<!-- --------------------------------------------category Vitamins & Supplements--------------------------------------------------------------------- -->
+    <div>
+      <h1  class=" border-b-2 border-green-200  w-3xs  my-5 font-extrabold text-lime-600">Vitamins & Supplements</h1>
+        
+    
+      <div class="flex overflow-x-auto gap-4">
+
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0  flex flex-col justify-between">
+          <img src="../../../assets/Blackmores.webp" class=" max-w-[50%] mx-auto" alt="Gaviscon">
+          <p class="text-center font-semibold">Blackmores</p>
+          <p class="text-center text-xs text-gray-500 font-light">สรรพคุณ: ช่วยสร้างภูมิต้านทาน ป้องกันหวัด เป็นสารต้านอนุมูลอิสระ และช่วยกระตุ้นการสร้างคอลลาเจนให้ผิวพรรณกระจ่างใส</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/fishoil.png" class=" max-w-[50%] mx-auto" alt="ยาแก้ไอ">
+          <p class="text-center font-semibold">Blackmores Fishoil</p>
+          <p class="text-center text-xs text-gray-500 font-light">อุดมไปด้วยโอเมก้า-3 (EPA และ DHA) ช่วยบำรุงสมอง เสริมความจำ ลดระดับไขมันไตรกลีเซอไรด์ในเลือด และลดอาการอักเสบของข้อต่อ</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+
+
+        <div class="border rounded-2xl w-sm  p-3 shadow-md shrink-0 bg-slate-50 flex flex-col justify-between">
+          <img src="../../../assets/Vitamin.webp" class=" max-w-[50%] mx-auto" alt="Smecta">
+          <p class="text-center font-semibold">Vitamin B Complex</p>
+          <p class="text-center text-xs text-gray-500 font-light">การใช้งาน:ช่วยเปลี่ยนอาหารให้เป็นพลังงาน เหมาะสำหรับคนที่ทำงานหนัก พักผ่อนน้อย สมองอ่อนล้า หรือมีอาการชาตามปลายมือปลายเท้า</p>
+          <a href="" class=" border rounded-2xl text-md font-bold  py-3 w-3xs bg-green-600 text-white block  text-center mt-5  mx-auto ">Buy Now!
+          </a>
+        </div>
+     </div>
 </div>
 
 
